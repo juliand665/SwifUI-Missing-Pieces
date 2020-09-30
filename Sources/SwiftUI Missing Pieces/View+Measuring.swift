2f9afd3f5ecc66db1d3a644bfef8e4d53ea2f7dd
Julian Dunskus <julian.dunskus@gmail.com>
2020-09-30 02:56:02 +0200

    add view measuring

---
+++ Sources/SwiftUI Missing Pieces/View+Measuring.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+public extension View {
+	/**
+	Measures this view, calling the given closure when the size changes (thus also initially).
+	
+	Be careful to not create infinite layout loops with this.
+	
+	- Parameters:
+		- onChange: a callback to call when the size changes.
+		- size: the new measured size of the view.
+	*/
+	func measured(onChange: @escaping (_ size: CGSize) -> Void) -> some View {
+		self
+			.background(
+				GeometryReader { proxy in
+					Color.clear.preference(key: SizeKey.self, value: proxy.size)
+				}
+				.hidden()
+			)
+			.onPreferenceChange(SizeKey.self) { onChange($0!) }
+	}
+}
+
+private typealias SizeKey = SimplePreferenceKey<SizeKeyMarker, CGSize>
+private enum SizeKeyMarker {}
